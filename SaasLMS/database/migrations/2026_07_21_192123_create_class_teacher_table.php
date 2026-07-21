2f096390129e11f828e77c89ce6e416dde6ddcee
Oshaq Naimat <oshaqnaimat15@gmail.com>
2026-07-22 00:22:19 +0500

    teacher dashboard

---
+++ SaasLMS/database/migrations/2026_07_21_192123_create_class_teacher_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+  public function up(): void
+{
+    Schema::create('class_teacher', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('class_room_id')->constrained()->cascadeOnDelete();
+        $table->foreignId('teacher_id')->constrained('users')->cascadeOnDelete();
+        $table->string('subject')->nullable();
+        $table->timestamps();
+        $table->unique(['class_room_id', 'teacher_id', 'subject']);
+    });
+}
+public function down(): void
+{
+    Schema::dropIfExists('class_teacher');
+}
+};
